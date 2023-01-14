c3022d5aec66802a099b88dad1cea04fd99e4d57
Jonathan Dahan <dahanj95@gmail.com>
2023-01-14 13:38:09 +0200

    Initial commit

---
+++ DpiFixer/Models/MappedImage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DpiFixer.Models
+{
+    public class MappedImage
+    {
+        private readonly Bitmap raw;
+
+        public MappedImage(string filePath)
+        {
+            raw = (Bitmap)Image.FromFile(filePath);
+        }
+
+        public bool HasRealDpi()
+        {
+            return (raw.Flags & (int)ImageFlags.HasRealDpi) > 0;
+        }
+
+        public bool HasLowDpi()
+        {
+            return (raw.HorizontalResolution < 96.0f) || (raw.VerticalResolution < 96.0f);
+        }
+
+        public void UpscaleDpi()
+        {
+            raw.SetResolution(96.0f, 96.0f);
+        }
+
+        public void SaveAs(string filePath)
+        {
+            raw.Save(filePath, ImageFormat.Jpeg);
+        }
+    }
+}
---
+++ DpiFixer/Program.cs
@@ -0,0 +1,42 @@
+﻿using DpiFixer.Models;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+string[] files = Directory.GetFiles(".\\", "*.jpg");
+
+Console.WriteLine($"Found {files.Length} files in the current folder.");
+
+if (files.Length == 0)
+{
+    return;
+}
+
+string dirName = Directory.GetCurrentDirectory();
+dirName = $".\\{Path.GetFileName(dirName)}_96"; ;
+
+if (!Directory.Exists(dirName))
+{
+    Directory.CreateDirectory(dirName);
+    Console.WriteLine($"Created directory {dirName}.");
+}
+
+Console.WriteLine("Working.... Please wait");
+int counter = 0;
+
+foreach (string file in files)
+{
+    var image = new MappedImage(file);
+
+    if (!image.HasRealDpi() || image.HasLowDpi())
+    {
+        image.UpscaleDpi();
+
+        string newName = $"{dirName}\\{file}";
+
+        image.SaveAs(newName);
+        counter++;
+    }
+}
+
+Console.WriteLine($"{counter} files have been fixed.");
+Console.ReadKey();
